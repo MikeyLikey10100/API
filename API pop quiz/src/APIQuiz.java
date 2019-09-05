2e7d7cd7388386f392b3402851b5ac9daef93d55
michael.Nguyen <michael.Nguyen@MU-C1-18.mullen.pvt.k12.co.us>
2019-09-05 11:24:40 -0600

    BoardQuiz

--- API pop quiz/src/APIQuiz.java
+++ API pop quiz/src/APIQuiz.java
@@ -5,13 +5,14 @@ public class APIQuiz
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
-
+				
+				Scanner userInput1 = new Scanner (System.in); 
+				Scanner userInput2 = new Scanner (System.in); 
 				System.out.println("pick two words please");
-				Scanner userInput = new Scanner (System.in); 
-				String word1 = userInput.nextLine();
-				String word2 = userInput.nextLine();
-				System.out.println(word1 + word2 .toLowerCase());
-			
+				String word1 = userInput1.nextLine();
+				String word2 = userInput2.nextLine();
+				System.out.println(word1.toLowerCase() + word2.toLowerCase()  );
+				
 			
 			}
 
